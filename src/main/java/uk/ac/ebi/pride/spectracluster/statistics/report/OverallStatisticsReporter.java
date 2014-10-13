5690055d8156896e850e21bead5d2d265dfb7e5d
Rui Wang <harry.r.wang@gmail.com>
2014-10-13 12:03:30 +0100

    changed the layout of the overall statistics report

--- src/main/java/uk/ac/ebi/pride/spectracluster/statistics/report/OverallStatisticsReporter.java
+++ src/main/java/uk/ac/ebi/pride/spectracluster/statistics/report/OverallStatisticsReporter.java
@@ -24,110 +24,81 @@ public class OverallStatisticsReporter implements IStatisticsReporter<OverallClu
         try {
             // number of cluster
             long numberOfClusters = stats.getNumberOfClusters();
-            output.append("Number of clusters: ");
-            output.append(numberOfClusters + "");
-            output.append("\n");
+            appendItem("Number of clusters:", numberOfClusters + "");
 
             // number of multi spectra clusters
             long numberOfMultiSpectraClusters = stats.getNumberOfMultiSpectraClusters();
-            output.append("Number of multi spectra clusters: ");
-            output.append(numberOfMultiSpectraClusters + "");
-            output.append("\n");
+            appendItem("Number of multi spectra clusters:", numberOfMultiSpectraClusters + "");
 
             // Ratio of multi spectra clusters
-            output.append("Ratio of multi spectra clusters: ");
-            output.append(String.format("%10.3f", (numberOfMultiSpectraClusters * 1.0f) / numberOfClusters).trim());
-            output.append("\n");
+            appendItem("Ratio of multi spectra clusters:", String.format("%10.3f", (numberOfMultiSpectraClusters * 1.0f) / numberOfClusters).trim());
 
             // number of reliable clusters
             long numberOfReliableClusters = stats.getNumberOfReliableClusters();
-            output.append("Number of reliable clusters: ");
-            output.append(numberOfReliableClusters + "");
-            output.append("\n");
+            appendItem("Number of reliable clusters:", numberOfReliableClusters + "");
 
             // ratio of reliable clusters
-            output.append("Ratio of reliable clusters: ");
-            output.append(String.format("%10.3f", (numberOfReliableClusters * 1.0f) / numberOfClusters).trim());
-            output.append("\n");
+            appendItem("Ratio of reliable clusters:", String.format("%10.3f", (numberOfReliableClusters * 1.0f) / numberOfClusters).trim());
 
             // number of single species reliable clusters
             long numberOfSingleSpeciesReliableClusters = stats.getNumberOfSingleSpeciesReliableClusters();
-            output.append("Number of single species reliable clusters: ");
-            output.append(numberOfSingleSpeciesReliableClusters + "");
-            output.append("\n");
+            appendItem("Number of single species reliable clusters:", numberOfSingleSpeciesReliableClusters + "");
 
             // ratio of single species reliable clusters
-            output.append("Ratio of single species reliable clusters: ");
-            output.append(String.format("%10.3f", (numberOfSingleSpeciesReliableClusters * 1.0f) / numberOfReliableClusters).trim());
-            output.append("\n");
+            appendItem("Ratio of single species reliable clusters:", String.format("%10.3f", (numberOfSingleSpeciesReliableClusters * 1.0f) / numberOfReliableClusters).trim());
 
             // number of two species reliable clusters
             long numberOfTwoSpeciesReliableClusters = stats.getNumberOfTwoSpeciesReliableClusters();
-            output.append("Number of two species reliable clusters: ");
-            output.append(numberOfTwoSpeciesReliableClusters + "");
-            output.append("\n");
+            appendItem("Number of two species reliable clusters:", numberOfTwoSpeciesReliableClusters + "");
 
             // ratio of single species reliable clusters
-            output.append("Ratio of two species reliable clusters: ");
-            output.append(String.format("%10.3f", (numberOfTwoSpeciesReliableClusters * 1.0f) / numberOfReliableClusters).trim());
-            output.append("\n");
+            appendItem("Ratio of two species reliable clusters:", String.format("%10.3f", (numberOfTwoSpeciesReliableClusters * 1.0f) / numberOfReliableClusters).trim());
 
             // number of multi species reliable clusters
             long numberOfMultiSpeciesReliableClusters = numberOfReliableClusters - numberOfSingleSpeciesReliableClusters;
-            output.append("Number of multi species reliable clusters: ");
-            output.append(numberOfMultiSpeciesReliableClusters + "");
-            output.append("\n");
+            appendItem("Number of multi species reliable clusters:", numberOfMultiSpeciesReliableClusters + "");
 
             // ratio of multi species reliable clusters
-            output.append("Ratio of multi species reliable clusters: ");
-            output.append(String.format("%10.3f", (numberOfMultiSpeciesReliableClusters * 1.0f) / numberOfReliableClusters).trim());
-            output.append("\n");
+            appendItem("Ratio of multi species reliable clusters:", String.format("%10.3f", (numberOfMultiSpeciesReliableClusters * 1.0f) / numberOfReliableClusters).trim());
 
             // number of species
             long numberOfSpecies = stats.getNumberOfSpecies();
-            output.append("Number of species: ");
-            output.append(numberOfSpecies + "");
-            output.append("\n");
+            appendItem("Number of species:", numberOfSpecies + "");
 
             // number of spectra
             long numberOfSpectra = stats.getNumberOfSpectra();
-            output.append("Number of spectra: ");
-            output.append(numberOfSpectra + "");
-            output.append("\n");
+            appendItem("Number of spectra:", numberOfSpectra + "");
 
             // number of clustered spectra
             long numberOfClusteredSpectra = stats.getNumberOfClusteredSpectra();
-            output.append("Number of clustered spectra: ");
-            output.append(numberOfClusteredSpectra + "");
-            output.append("\n");
+            appendItem("Number of clustered spectra:", numberOfClusteredSpectra + "");
 
             // ratio of clustered spectra
-            output.append("Ratio of clustered spectra: ");
-            output.append(String.format("%10.3f", (numberOfClusteredSpectra * 1.0f) / numberOfSpectra).trim());
-            output.append("\n");
+            appendItem("Ratio of clustered spectra:", String.format("%10.3f", (numberOfClusteredSpectra * 1.0f) / numberOfSpectra).trim());
 
             // number of not clustered spectra
             long numberOfNotClusteredSpectra = numberOfSpectra - numberOfClusteredSpectra;
-            output.append("Number of not clustered spectra: ");
-            output.append(numberOfNotClusteredSpectra + "");
-            output.append("\n");
+            appendItem("Number of not clustered spectra:", numberOfNotClusteredSpectra + "");
 
             // ratio of not clustered spectra
-            output.append("Ratio of not clustered spectra: ");
-            output.append(String.format("%10.3f", (numberOfNotClusteredSpectra * 1.0f) / numberOfSpectra).trim());
-            output.append("\n");
+            appendItem("Ratio of not clustered spectra:", String.format("%10.3f", (numberOfNotClusteredSpectra * 1.0f) / numberOfSpectra).trim());
 
             // number of assays
             // todo: assay accession is not included yet
 
             // number of projects
             long numberOfProjects = stats.getNumberOfProjects();
-            output.append("Number of projects: ");
-            output.append(numberOfProjects + "");
-            output.append("\n");
+            appendItem("Number of projects:", numberOfProjects + "");
 
         } catch (IOException e) {
             throw new IllegalStateException("Failed to report overall statistics", e);
         }
+    }
+
+    private void appendItem(String name, String value) throws IOException {
+        output.append(name);
+        output.append("\n");
+        output.append(value);
+        output.append("\n\n");
     }
 }
