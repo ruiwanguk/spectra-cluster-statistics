cc913a60e928cc542f07769d68ce88b845cbd792
Rui Wang <harry.r.wang@gmail.com>
2014-10-13 10:30:25 +0100

    fixed the problem with shell script irreplaceable jar file name

--- script/runClusterStatistics.sh
+++ script/runClusterStatistics.sh
@@ -12,8 +12,9 @@ CLUSTER_STAT_OUTPUT_FILE=$3
 JOB_NAME="PRIDE-CLUSTER-STATISTICS"
 # memory limit in MGb
 MEMORY_LIMIT=10000
-
+# executable jar file
+EXE_JAR=${project.build.finalName}.jar
 
 ##### RUN it on the production LSF cluster #####
 ##### NOTE: you can change LSF group to modify the number of jobs can be run concurrently #####
-bsub -M ${MEMORY_LIMIT} -R "rusage[mem=${MEMORY_LIMIT}]" -q production-rh6 -g /pride_cluster_loader -u rwang@ebi.ac.uk -J ${JOB_NAME} java -Xmx${MEMORY_LIMIT}m -jar ${project.build.finalName}.jar ${CLUSTER_FILE_DIRECTORY} ${OVERALL_STAT_OUTPUT_FILE} ${CLUSTER_STAT_OUTPUT_FILE}
+bsub -M ${MEMORY_LIMIT} -R "rusage[mem=${MEMORY_LIMIT}]" -q production-rh6 -g /pride_cluster_loader -u rwang@ebi.ac.uk -J ${JOB_NAME} java -Xmx${MEMORY_LIMIT}m -jar ${EXE_JAR} ${CLUSTER_FILE_DIRECTORY} ${OVERALL_STAT_OUTPUT_FILE} ${CLUSTER_STAT_OUTPUT_FILE}
